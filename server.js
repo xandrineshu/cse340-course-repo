9841260b145129149d55fe82e597b05d0c203d84
xandrineshu <jconcha1@byupathway.edu>
2026-09-04 17:18:42 +0800

    finished W01 Learning Activity: EJS Templates

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-// Week 1 Assignment
-// console.log("Hello Node.js!");
-
-// Week 1 Learning Assignment: Node.js
-// import express from "express";
-
-// const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-// const PORT = process.env.PORT || 3000;
-
-/// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.send("Hello from Express (using nodemon)!");
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running at http://127.0.0.1:${PORT}`);
-//     console.log(`Environment: ${NODE_ENV}`);
-// });
-
-
 import express from "express";
 
 import { fileURLToPath } from 'url';
@@ -32,6 +11,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 /**
   * Configure Express middleware
   */
@@ -42,16 +27,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
   * Routes
   */
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
 });
 
-app.get('/organizations', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
 });
 
-app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
 });
 
 app.listen(PORT, () => {

22e622f48084f987a17e37a8d64be31b0d62fb7a
xandrineshu <jconcha1@byupathway.edu>
2026-09-04 16:02:53 +0800

    finish week 1 learning activity: Node.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 
 import express from "express";
 
-const NODE_ENV = "production";
-const PORT = 3000;
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express (using nodemon)!!");
+    res.send("Hello from Express (using nodemon)!");
 });
 
 app.listen(PORT, () => {

c573b62ae23a69df0c61eb6f2bff159fa931b430
xandrineshu <jconcha1@byupathway.edu>
2026-09-09 14:46:27 +0800

    Fixed HTML + CSS problem // W02 Learning Activity: Displaying Database results in EJS Completed

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import path from 'path';
 
 import { testConnection } from './src/models/db.js';
 
+import { getAllOrganizations } from './src/models/organizations.js';
+
 dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -38,8 +40,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {

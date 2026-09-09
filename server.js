674f4062d2636569cc4b64b2b7a5edee662a95a3
xandrineshu <jconcha1@byupathway.edu>
2026-09-09 14:22:26 +0800

    W02 Learning Activity: Connecting to Databases from Node.js Completed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+import { testConnection } from './src/models/db.js';
+
 dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -51,8 +53,13 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
 

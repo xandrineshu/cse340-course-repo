1dd761cc17a54d3a4f35d865b1fbc14f7d71e7b6
xandrineshu <jconcha1@byupathway.edu>
2026-09-14 09:49:29 +0800

    W03 Learning Activity: Middleware // Complete

--- server.js
+++ server.js
@@ -32,6 +32,20 @@ app.set('views', path.join(__dirname, 'src/views'));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 /**
   * Routes
   */
